57083dc8187ecc986c667594d9d2ca52933f7e8e
shrief <shrifzain86@gmail.com>
2025-04-08 20:11:25 +0000

    master

---
+++ Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/api/hello", () => 
+{
+    return Results.Ok(new { Message = "Hello World from .NET 6!" });
+});
+
+app.Run();
